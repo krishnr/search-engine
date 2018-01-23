bb21ccfd88e87da36b0e894632e3bd80a7824785
Krishn Ramesh <krishnr@gmail.com>
2018-01-22 22:52:58 -0500

    added comments

--- A1/doc-finder.py
+++ A1/doc-finder.py
@@ -6,6 +6,7 @@ import os
 import pickle
 import gzip
 
+# Error message if incorrect number of inputs (ie. not 3 inputs)
 if len(sys.argv) != 4:
     print "Please provide 3 arguments: the location of the metadata index, the string 'docno' or 'id', and the internal integer id or the DOCNO"
     exit()
@@ -14,35 +15,45 @@ metadata_folder = sys.argv[1]
 id_type = sys.argv[2]
 doc_id = sys.argv[3]
 
+# Path to metadata dictionary pickle
 metadata_file = metadata_folder + '/metadata_dict.p'
 
+# if metadata dictionary hasn't been created
 if not os.path.exists(metadata_file):
-    print "Path to metadata is invalid!"
+    print "Path to metadata dictionary is invalid!"
 
+# loading the metadata dictionary pickle
 with open(metadata_file, 'rb') as handle:
     metadata_dict = pickle.load(handle)
 
+# loading the id_to_docno dictionary pickle
 with open(metadata_folder + '/pickles/id_to_docno.p', 'rb') as handle:
     id_to_docno = pickle.load(handle)
 
+# loading the docno_to_id dictionary pickle
 with open(metadata_folder + '/pickles/docno_to_id.p', 'rb') as handle:
     docno_to_id = pickle.load(handle)
 
+# if arg 2 was docno, map to id
 if id_type.lower() == "docno":
     doc_id = docno_to_id[doc_id]
+# else if arg 2 was invalid
 elif id_type.lower() != "id":
     print "ERROR: Argument 2 must be one of 'docno' or 'id'"
     exit()
 
+# loading doc metadata from the metadata dictionary
 metadata = metadata_dict[int(doc_id)]
 docno = metadata['docno']
 date = metadata['date']
 headline = metadata['headline']
 
+# finding the raw document using date and doc_id
 doc_file = metadata_folder+ "/docs/" + date + "/" + str(doc_id) + ".txt.gz"
 with gzip.open(doc_file, 'rb') as doc:
     raw_text = doc.read()
 
+# printing output
 print "docno: " + docno
 print "internal id: " + str(doc_id)
 print "date: " + date
--- A1/metadata-engine.py
+++ A1/metadata-engine.py
@@ -7,6 +7,7 @@ import gzip
 import re
 import pickle
 
+# Error message if incorrect number of inputs (ie. not 2 inputs)
 if len(sys.argv) != 3:
     print "Please provide 2 arguments: the location of latimes.gzip and the location for the metadata index"
     exit()
@@ -14,12 +15,12 @@ if len(sys.argv) != 3:
 gz_file = sys.argv[1]
 metadata_folder = sys.argv[2]
 
-# "./latimes.gz"
+# Error message if path to gz file is invalid
 if not os.path.exists(gz_file):
     print "Path to dataset is invalid!"
     exit()
 
-# "./metadata.txt"
+# Make directory if doesn't exist, otherwise throw error
 if not os.path.exists(metadata_folder):
     print "Making directory: " + metadata_folder
     os.makedirs(metadata_folder)
@@ -27,24 +28,32 @@ else:
     print "Directory already exists!"
     exit()
 
+# dictionary to store metadata
 metadata_dict = {}
+
+# mappings from id <=> docno
 id_to_docno = {}
 docno_to_id = {}
 
+# method to save the raw text of the doc
 def save_doc(doc_content, date, doc_id):
+    # make 'docs' folder if doesn't exist
     if not os.path.exists(metadata_folder + '/docs'):
         os.makedirs(metadata_folder + '/docs')
 
+    # make date directory if doesn't exist
     directory = metadata_folder + "/docs/" + date
-    
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    # store raw doc with its doc_id
     filename = directory + "/" + str(doc_id) + '.txt.gz'
 
+    # save raw text as a compressed text file
     with gzip.open(filename, 'wb') as doc_file:
         doc_file.write(doc_content)
 
+# method to extract doc metadata
 def extract_metadata(doc_content, doc_id):
     # extracting docno
     docno_regex = re.compile(r"<DOCNO>(.+)</DOCNO>", re.DOTALL)
@@ -75,14 +84,18 @@ def extract_metadata(doc_content, doc_id):
     else:
         print "ERROR: date not parsed for doc_id %s" % doc_id
 
+    # save to metadata dictionary using doc_id as the key
     metadata_dict[doc_id] = {
         "docno": docno,
         "headline": headline,
         "date": date
     }
 
+    # record mapping for id <=> docno
     id_to_docno[doc_id] = docno
     docno_to_id[docno] = doc_id
+
+    # save the raw text in the proper directory
     save_doc(doc_content, date, doc_id)
 
 # iterating through file
@@ -90,22 +103,28 @@ with gzip.open(gz_file, 'rb') as f:
     doc_content = ""
     in_doc = False
     doc_id = 0
+
+    # parse gz file line by line
     for line in f:
+        # entering a new doc
         if "<DOC>" in line:
             in_doc = True
     
         if in_doc:
             doc_content += line
         
+        # leaving a doc
         if "</DOC>" in line:
             in_doc = False
             extract_metadata(doc_content, doc_id)
             doc_id += 1
             doc_content = ""
 
+# make pickles folder if doesn't exist
 if not os.path.exists(metadata_folder + '/pickles'):
     os.makedirs(metadata_folder + '/pickles')
 
+# store all dictionaries as pickles
 pickle.dump(metadata_dict, open(metadata_folder + '/metadata_dict.p', 'wb'))
 pickle.dump(id_to_docno, open(metadata_folder + '/pickles/id_to_docno.p', 'wb'))
 pickle.dump(docno_to_id, open(metadata_folder + '/pickles/docno_to_id.p', 'wb'))
